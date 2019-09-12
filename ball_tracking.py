6573cddd188b42d5d4dfd15317c6e35b4375b588
Manuchkhr Adina-Zada <manu.adina-zada@hotmail.com>
2019-09-12 13:53:47 +0800

    Saving progress

--- ball_tracking.py
+++ ball_tracking.py
@@ -52,7 +52,7 @@ while True:
     mark = cv2.dilate(mask, None, iterations = 2)
 
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
+    cnts = imutils.grab_contours(cnts) # I don't want to use imutils.
     center = None
 
 
@@ -61,7 +61,7 @@ while True:
         c = max(cnts, key=cv2.contourArea)
         ((x, y), radius) = cv2.minEnclosingCircle(c)
         M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"])) # Thats how you find the center
 
         # Only proceed if the radius is over the minimum size.
         if radius > 10:
@@ -69,6 +69,30 @@ while True:
             cv2.circle(frame, center, 5, (0, 255, 255), -1)
 
     pts.append(center)
+
+    for i in range(1, len(pts)):
+        if pts[i - 1] is None or pts[i] is None:
+            continue
+
+        thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+        cv2.line(frame, pts[i - 1], pts[i] (0,0,255), thickness)
+
+
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+        break
+
+
+if not args.get("video", False):
+    vs.stop()
+
+else:
+    vs.release()
+
+cv2.destroyAllWindows()
+
 
 
 
